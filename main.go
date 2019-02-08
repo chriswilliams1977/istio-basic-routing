1230e3ff42f0722aeaa95298f13d2d3db67cc1c4
chriswilliams1977 <chriswilliams1977@gmail.com>
2019-02-08 13:50:46 +0100

    updating go container files

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	var PORT string
+	if PORT = os.Getenv("PORT"); PORT == "" {
+		PORT = "3001"
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World from path: %s\n", r.URL.Path)
+	})
+	http.ListenAndServe(":"+PORT, nil)
 }
